32cd1fa84e06c8cbc43ede9cc1ceaf24b3b50c39
Weikoi <huangzengrui@yahoo.com>
2020-08-27 20:40:46 +0800

    add uml pic

--- src/Beverage.py
+++ src/Beverage.py
@@ -3,12 +3,6 @@ class Beverage(object):
     def __init__(self, spec):
         self.spec = spec
 
-    def get_prop(self, prop_name):
-        return self.spec[prop_name]
-
-    def get_all_props(self):
-        return self.spec.items()
-
 
 class Coffee(Beverage):
     def __init__(self, coffee_spec):
